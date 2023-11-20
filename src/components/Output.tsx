3498ed26d54f2246bdad88a0cb36402c4b684dc5
ramzi hannachi <rhannachi1991@gmail.com>
2023-11-20 17:25:33 +0100

    Feat - add copy function - Output component

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,10 +1,40 @@
 'use client'
 
+import { useState } from 'react'
+
 export const Output = ({ label }: { label: string }) => {
+  const [isCopied, setIsCopied] = useState(false)
+
+  const copyTextToClipboard = async (text: string) => {
+    if ('clipboard' in navigator) {
+      return await navigator.clipboard.writeText(text)
+    } else {
+      return document.execCommand('copy', true, text)
+    }
+  }
+
+  const handleOnClick = () => {
+    copyTextToClipboard(label)
+      .then(() => {
+        setIsCopied(true)
+        setTimeout(() => {
+          setIsCopied(false)
+        }, 3500)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
-    <div className='flex flex-row py-2 bg-gray-50 px-2 w-full justify-end'>
-      <div className='text-center font-semibold text-blue-600 w-full'>{label}</div>
-      <div className='absolute cursor-pointer'>📋</div>
-    </div>
+    <>
+      <div className='flex flex-row py-2 bg-gray-50 px-1 w-full justify-end'>
+        <div className='text-center font-semibold text-blue-600 w-full'>{label}</div>
+        <div onClick={handleOnClick} className='absolute cursor-pointer '>
+          📋
+        </div>
+      </div>
+      <span className='text-sm text-green-600 font-semibold'>{isCopied && 'Copied !'}</span>
+    </>
   )
 }

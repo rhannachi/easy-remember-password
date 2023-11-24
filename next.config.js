baea4f4027e51c61917e57f98ded82c2975794ae
ramzi hannachi <rhannachi1991@gmail.com>
2023-11-24 14:19:01 +0100

    Fix - next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
+//
+// module.exports = withBundleAnalyzer({
+//   reactStrictMode: true,
+//   output: 'export',
+//   experimental: {
+//     optimizePackageImports: ['hash-wasm'],
+//   },
+// })
 
-module.exports = withBundleAnalyzer({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   experimental: {
     optimizePackageImports: ['hash-wasm'],
   },
-})
+}
+module.exports = nextConfig
